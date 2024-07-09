1bc812494755f2e0dd823c1e719b84dc65e53385
Anmol Srivastava <144585099+MrAnmolSrivastava@users.noreply.github.com>
2024-07-09 09:59:53 +0530

    Update rename.java

--- rename.java
+++ rename.java
@@ -3,7 +3,9 @@ public class rename{
     public static void main(String[] args) {
       
        Scanner sc =  new Scanner(System.in);
+        // Asking the user to enter the present age
        System.out.println("enter your age");
+        // age is used as an integer variable
        int age = sc.nextInt();
        System.out.println("your age is " +age); 
 
